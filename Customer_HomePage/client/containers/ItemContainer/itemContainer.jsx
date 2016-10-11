e59054d32daadb8ce3797f73e7cc77079131b4be
wangyafei <wangyafei@startimes.com.cn>
2016-10-11 21:50:39 +0800

    modify homePage

--- Customer_HomePage/client/containers/ItemContainer/itemContainer.jsx
+++ Customer_HomePage/client/containers/ItemContainer/itemContainer.jsx
@@ -2,8 +2,33 @@
 import React from 'react';
 import Item from '../../components/Item/index'
 import AddButton from '../../components/AddButton/addButton'
+import MasonryMixin from './MasonryMixin'
 require('./index.css');
 
+var masonryOptions = {
+    transitionDuration: 0
+};
+
+var ItemContainer = React.createClass({
+    mixins:[MasonryMixin(React)('masonryContainer',masonryOptions)],
+
+    render() {
+        let props = this.props.items;
+        let items = props.map((item, index) => {
+            return <Item item={item} key={index} isSliderItem={false}>
+                <AddButton item={item}/>
+            </Item>
+        });
+        return (
+            <div className="itemContainer" ref="masonryContainer">
+                {items}
+            </div>
+        )
+    }
+});
+
+module.exports = ItemContainer;
+/*
 export default class ItemContainer extends React.Component {
     constructor(props) {
         super(props)
@@ -22,4 +47,4 @@ export default class ItemContainer extends React.Component {
             </div>
         )
     }
-}
+}*/
