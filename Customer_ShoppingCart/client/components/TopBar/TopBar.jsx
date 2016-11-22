b243e71ed6d9a666878756fdb92951002c906fc9
jessie <jessiezhang@izhida.cn>
2016-11-22 18:53:12 +0800

    购物车logo，因为配置有点问题，所以改完之后还没检查

--- Customer_ShoppingCart/client/components/TopBar/TopBar.jsx
+++ Customer_ShoppingCart/client/components/TopBar/TopBar.jsx
@@ -16,7 +16,7 @@ export default class TopBar extends React.Component {
         const props = this.props;
         return(
             <div className='headContainer'>
-                <img src={require('./images/MaxSpot.png')} className='logo'/>
+                <img src={require('./images/logo.png')} className='logo'/>
                 <span ref='searchContinue' className='searchContinue'
                       onClick={this.searchClick.bind(this)}>再去逛逛</span>
             </div>
