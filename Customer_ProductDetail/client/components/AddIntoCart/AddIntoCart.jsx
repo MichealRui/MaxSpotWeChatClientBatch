ad7fc759941a8c7fe33a3aee00b98b1f02e798b9
jessie <jessiezhang@izhida.cn>
2016-11-23 18:10:53 +0800

    商品详情，售罄样式修改

--- Customer_ProductDetail/client/components/AddIntoCart/AddIntoCart.jsx
+++ Customer_ProductDetail/client/components/AddIntoCart/AddIntoCart.jsx
@@ -11,6 +11,8 @@ export default class AddIntoCart extends React.Component {
 
 	render(){
 		let props = this.props;
+		let btn_text = props.productCount > 0 ? '加入购物车': '售 罄'
+		let btn_class = props.productCount > 0 ? 'addProduct': 'addProduct empty'
 		return(
 			<div className="addIntoCart">
 				<div className='wrapper'>
@@ -18,7 +20,7 @@ export default class AddIntoCart extends React.Component {
 					<p className='font12'>库存 {props.productCount}件</p>
 				</div>
 				<div className='buttonArea'>
-					<Button buttonClassName='addProduct' buttonClick={()=>props.addIntoCartClick()} buttonText={'加入购物车'}/>
+					<Button buttonClassName={btn_class} buttonClick={()=>props.addIntoCartClick()} buttonText={btn_text}/>
 				</div>
 			</div>
 		);
