7c5735eb581aee4ecedbbacce6cf1ea8cd1fe363
Ruibing <ruibing@Ruibings-MacBook-Pro.local>
2016-11-24 19:41:46 +0800

    fix bug of brand

--- Customer_BrandDetail/client/components/timer/timer.jsx
+++ Customer_BrandDetail/client/components/timer/timer.jsx
@@ -17,7 +17,6 @@ export default class timer extends React.Component {
 		const item = props.iteminfo;
 		return (
 			<div className={item.total ? "timer" : "timer empty"} onClick={this.toCart(this)}>
-			<div className="timer">
 				<div className="number">{item.total}</div>
 				<div className="lasttime">
 					<p>剩余时间</p>
