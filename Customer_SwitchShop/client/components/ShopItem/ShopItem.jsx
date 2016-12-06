dbc275548da2af9098accec4a1b0af713a69edc5
jessie <jessiezhang@izhida.cn>
2016-12-06 17:45:03 +0800

    switch shop css

--- Customer_SwitchShop/client/components/ShopItem/ShopItem.jsx
+++ Customer_SwitchShop/client/components/ShopItem/ShopItem.jsx
@@ -20,8 +20,8 @@ export default class ShopItem extends React.Component {
 			<div className='shopItem' onClick={this.shopClick.bind(this)}>
 				<img src={ENV.domain + '/' + props.imagePath} className='shopImg' />
 				<div className='infoArea'>
-					<h1 className='font16'>{props.name}<span className="font14">i</span></h1>
-					<p className='font14'>{props.address}</p>
+					<h1 className='font15'>{props.name}<span className="font14">i</span></h1>
+					<p className='font12'>{props.address}</p>
 				</div>
 				<span className=""> <img src={require('./images/arrow.png')} alt=""/> </span>
 				{/*<span className='fa fa-angle-right orderAddressArrow font24'></span>*/}
