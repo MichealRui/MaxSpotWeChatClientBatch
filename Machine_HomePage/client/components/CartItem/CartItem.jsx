1689dd97fefbd09b0e1e432fba35da4c7a212241
cabbage <403791823@qq.com>
2016-12-12 01:39:56 +0800

    首页 商品弹窗样式， 未完成

--- Machine_HomePage/client/components/CartItem/CartItem.jsx
+++ Machine_HomePage/client/components/CartItem/CartItem.jsx
@@ -33,8 +33,35 @@ export default class CartItem extends React.Component{
         )
     }
 
+    getAtts(attributes){
+        let def = <span></span>
+        var atts = def;
+        if(attributes && attributes.length > 0){
+            atts = attributes.map((item,index)=>{
+                if(item.value){
+                    return item.value + item.unit;
+                }else{
+                    return ''
+                }
+            }).reduce((pre,next)=>{
+                if(pre == '' && next == ''){
+                    return ''
+                }else{
+                    return pre + next + ' '
+                }
+            },'')
+        }
+        if(atts == ''){
+            atts = def
+        }
+        return atts;
+    }
+
     render() {
         let props = this.props.item;
+        // console.log(props)
+        var atts = this.getAtts(props.attributes)
+        console.log(atts)
         return (
             <div className="cart-item my-item">
                 <div className="item-pic">
@@ -43,19 +70,33 @@ export default class CartItem extends React.Component{
                 <h2 className="item-name">
                     <span>{props.brandName}</span>
                     <span>{props.shortName}</span>
+                    <span className=' categoryName font18'>
+                        {atts}
+                    </span>
                 </h2>
-                <h3 className="item-price clearfix">
-                    <span className="final-price">¥{props.sellprice /100 || 0}</span>
-                    {/*<span class="market-price">市场价¥126</span>*/}
+                <h3 className="item-price">
+                    <span className="final-price">{props.sellprice /100 || 0}<span className="font20">元</span></span>
+                    <span className="market-price font20 hide">市场价¥126</span>
                 </h3>
                 <div className="item-panel clearfix">
-                    <div className="counting clearfix">
+                    <div className={"counting undershop font20 hide"}>此商品已售罄</div>
+                    <div className={"counting clearfix "}>
                         <a className="btn-minus" disabled={props.count == 1} onClick={() => this.decrease.bind(this)()}>-</a>
                         <span type="text" className="quantity" value={props.count} readOnly="readOnly">{props.count}</span>
                         <a className="btn-plus" disabled={ props.quantity <= props.count} onClick={() => this.addItem.bind(this)()}>+</a>
                     </div>
                     <a className="trash" onClick={() => this.removeItem.bind(this)()}>Remove this item!</a>
                 </div>
+                <div className="layer hide">
+                    <div className="dialog">
+                        <div className="font26 del">删除此商品？</div>
+                        <div className="btn clearfix">
+                            <div className="btn_yes font30">确定</div>
+                            <div className="btn_no font30">取消</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="layer hide"></div>
             </div>
         )
     }
--- Machine_HomePage/client/containers/CartBottom/CartBottom.jsx
+++ Machine_HomePage/client/containers/CartBottom/CartBottom.jsx
@@ -41,14 +41,14 @@ export default class CartBottom extends React.Component {
 
                 <div className="cart-my-total">
                     <div className="my-total-box">
-                        {/*<div className="price-one-line clearfix">*/}
-                            {/*<span className="price-name">商品总金额</span>*/}
-                            {/*<span className="price-num">¥2321</span>*/}
-                        {/*</div>*/}
-                        {/*<div className="price-one-line clearfix">*/}
-                            {/*<span className="price-name">优惠总计</span>*/}
-                            {/*<span className="price-num">¥-321</span>*/}
-                        {/*</div>*/}
+                        <div className="price-one-line clearfix">
+                            <span className="price-name">商品总金额</span>
+                            <span className="price-num">¥2321</span>
+                        </div>
+                        <div className="price-one-line clearfix">
+                            <span className="price-name">优惠总计</span>
+                            <span className="price-num">¥-321</span>
+                        </div>
                         {/*<div className="price-one-line clearfix">*/}
                             {/*<span className="price-name">优惠总计</span>*/}
                             {/*<span className="price-num">¥-300</span>*/}
@@ -61,11 +61,16 @@ export default class CartBottom extends React.Component {
                             <span className="price-name">应付金额</span>
                             <span className="price-num total-pay">¥{this.props.totalPrice}</span>
                         </div>
+                        <div className="button font34">清空购物袋</div>
                     </div>
                 </div>
 
                 <div className="cart-weixin-pay" onClick={() => this.onClickQr.bind(this)()}>
                     <img className="wx-pic" src={require("./images/weixinpay.png")} width="224" alt="微信支付" />
+                    <div className="text font20">
+                        <img src={require('./images/backborder.png')} alt=""/>
+                        <span className="font20">部分商品缺货或已下架，请编辑购物袋</span>
+                    </div>
                 </div>
             </div>
 
