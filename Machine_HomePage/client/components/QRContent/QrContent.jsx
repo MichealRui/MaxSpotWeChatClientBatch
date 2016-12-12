2701e3e5ade9be9322e4be6e3cce5bb533009824
jessie <jessiezhang@izhida.cn>
2016-12-12 19:40:40 +0800

    扫描二维码样式

--- Machine_HomePage/client/components/QRContent/QrContent.jsx
+++ Machine_HomePage/client/components/QRContent/QrContent.jsx
@@ -1,6 +1,7 @@
 'use strict';
 import React from 'react'
 import ReactQrCode from 'qrcode.react'
+require('./index.css')
 
 export default class QrContent extends React.Component {
     constructor(props) {
@@ -27,9 +28,38 @@ export default class QrContent extends React.Component {
 
     render () {
         let qr = this.props.qr;
+        console.log(qr)
+        let size = 270;
         return (
-            <div onClick={() => this.props.setCartTaking()}>
-                <ReactQrCode value={qr}/>
+            <div className="qrcode" onClick={() => this.props.setCartTaking()}>
+                <div className="code clearfix"><ReactQrCode size={size} value={qr}/></div>
+                <div className="payInfo">
+                    <div className="title font48">微信扫一扫支付<span className="price"><span className="font32">￥</span>120</span></div>
+                    <div className="text font34">如何取货</div>
+                    <div className="iconInfo clearfix">
+                        <div className="stepInfo">
+                            <div className="icon"><img src={require('./image/4_1.png')} alt=""/></div>
+                            <div className="explain font24">
+                                <p>第一步：</p>
+                                <p>使用手机，进入手机微信</p>
+                            </div>
+                        </div>
+                        <div className="stepInfo">
+                            <div className="icon"><img src={require('./image/4_2.png')} alt=""/></div>
+                            <div className="explain font24">
+                                <p>第二步：</p>
+                                <p>点击微信右上方菜单，找到“扫一扫”</p>
+                            </div>
+                        </div>
+                        <div className="stepInfo">
+                            <div className="icon"><img src={require('./image/4_3.png')} alt=""/></div>
+                            <div className="explain font24">
+                                <p>第三步：</p>
+                                <p>打开微信扫一扫，扫描屏幕上的二维码</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
