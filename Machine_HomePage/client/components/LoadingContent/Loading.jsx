8c12ed8f1574a62562d070136a4cd8ccc25f439c
jessie <jessiezhang@izhida.cn>
2016-12-13 15:27:59 +0800

    loading css

--- Machine_HomePage/client/components/LoadingContent/Loading.jsx
+++ Machine_HomePage/client/components/LoadingContent/Loading.jsx
@@ -1,6 +1,6 @@
 'use strict';
 import React from 'react'
-
+require('./index.css')
 export default class Loading extends React.Component {
     constructor(props) {
         super(props)
@@ -9,7 +9,14 @@ export default class Loading extends React.Component {
     render() {
         return(
             <div>
-                Loading
+                <div className="loading">
+                    <img src={require('./image/loading.gif')} alt=""/>
+                    <p className="text font34">
+                        <span> 正在生成订单……</span>
+
+                    </p>
+
+                </div>
             </div>
         )
     }
