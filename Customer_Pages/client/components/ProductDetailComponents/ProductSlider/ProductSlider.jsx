dbf650e9431f61c2fb917e9ae72931bc0adeae6c
cabbage <403791823@qq.com>
2017-03-17 18:22:52 +0800

    bug

--- Customer_Pages/client/components/ProductDetailComponents/ProductSlider/ProductSlider.jsx
+++ Customer_Pages/client/components/ProductDetailComponents/ProductSlider/ProductSlider.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import SwiperComponent from '../../CommoonComponents/Swiper/index';
+import util from '../../../util/WeChatUtil';
 require('./index.css');
 
 export default class ProductSlider extends React.Component {
@@ -9,15 +10,6 @@ export default class ProductSlider extends React.Component {
 		super(props);
 	}
 
-	getMiddlePic(path) {
-		let particial = path.split('.');
-		if(particial.length == 2) {
-			particial[0] = particial[0] + '_middle'
-			return particial.join('.')
-		} else {
-			path
-		}
-	}
 
 	render(){
 		let swiperConfig = {
@@ -28,11 +20,16 @@ export default class ProductSlider extends React.Component {
 		let style = {}
 		let props = this.props;
 		let productImgs = props.sliderData.images;
+		let defaultImg = DEFALUT_INFO.defaultImg;
+		// let domain = ENV.domain;
+		let domain = "http://114.215.143.97/";
 		let sliders = productImgs.map(
 			(img,index)=>{
-				return (
-					<img width="60%" key={index} style={style} src={ ENV.domain + this.getMiddlePic(img) || require('./images/default.png')} alt=""/>
-				)
+                return (
+                    img ? <img width="60%" key={index} style={style} src={ domain + util.getMiddlePic(img) } alt=""/>
+                    :
+                    <img width="60%" key={index} style={style} src={defaultImg} alt=""/>
+                )
 			}
 		)
 		return (
--- Customer_Pages/client/components/ProductDetailComponents/StoreIntro/StoreIntro.jsx
+++ Customer_Pages/client/components/ProductDetailComponents/StoreIntro/StoreIntro.jsx
@@ -3,6 +3,7 @@
 require ('./index.css');
 import React from 'react';
 import {Link} from 'react-router'
+import util from '../../../util/WeChatUtil';
 export default class StoreIntro extends React.Component {
 	constructor(props){
 		super(props);
@@ -18,14 +19,22 @@ export default class StoreIntro extends React.Component {
 		let props = this.props;
 		let brand = props.brandData;
 		let brandId = brand.id;
-		let storeId = this.props.storeId
+		let storeId = this.props.storeId;
+		let defaultImg = DEFALUT_INFO.defaultImg;
+		// let domain = ENV.domain;
+		let domain = "http://114.215.143.97";
 		return (
 			<div>
 				<p className='storeInfo font12'>{props.productData.description}</p>
 				<div className='storeIntroWrap'>
 					<Link to={"/brand/"+storeId+"/"+brandId}>
 						<div className='storeIntro'>
-							<img src={props.brandData.imagePath || require('./images/default.png')} className='storeImg'/>
+							{
+								props.brandData.imagePath ? <img src={domain + util.getMiddlePic(props.brandData.imagePath)} className='storeImg'/>
+															:
+									<img src={defaultImg} className='storeImg'/>
+
+							}
 							<div className="storeIntroInfo">
 								<h1 className='font14'>{props.brandData.name}</h1>
 								<p className='font12'>{props.brandData.story}</p>
@@ -47,7 +56,7 @@ StoreIntro.defaultProps = {
 		description : ''
 	},
 	brandData : {
-		imagePath :[],
+		imagePath :'',
 		name : '',
 		story: '',
 		id : 0
