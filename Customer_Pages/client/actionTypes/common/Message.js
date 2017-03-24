6cdaac35a2ee9bddadd8c8b9c4d2cac07ea8d28f
Ruibing <ruibing@Ruibings-MBP.lan>
2017-03-24 11:37:36 +0800

    rm pages

--- Customer_Pages/client/actionTypes/common/Message.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by ruibing on 17/1/13.
- */
-export const SET_MESSAGE = 'SET_MESSAGE';
--- Customer_Pages/client/env/test.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by ruibing on 16/11/2.
- */
-module.exports = {
-    domain: 'http://test.mjitech.com'
-}
