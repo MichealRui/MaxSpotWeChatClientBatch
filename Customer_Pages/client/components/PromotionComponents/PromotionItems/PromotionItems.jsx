d078806500ba0961082413eaa36ef1e658f13542
cabbage <403791823@qq.com>
2017-04-01 16:34:23 +0800

    bug

--- Customer_Pages/client/components/PromotionComponents/PromotionItems/PromotionItems.jsx
+++ Customer_Pages/client/components/PromotionComponents/PromotionItems/PromotionItems.jsx
@@ -43,35 +43,40 @@ export default class PromotionItems extends React.Component{
         let itemTag = itemData.tags ?  <div className="activityItemsTags font12">{itemData.tags}</div>:'';
         let itemDesc = props.type == 1 ? <div className="activityDesc">{itemData.description}</div> : '';
         let attr = this.getAttr(itemData.attributes)
-        return(
-            <div className={"activityItems "+(itemData.quantity > 0 ? '' : 'sellout')}>
-                <div className="activityItem">
-                    <div className="activityItemsImg">
-                        {
-                            itemData.imagePath ?
-                            <img src={ domain + util.getMiddlePic(itemData.imagePath) }/> :
-                            <img src={defProductImg}/>
+        if(itemData.status == 2){
+            return null
+        }else{
+            return(
+                <div className={"activityItems "+(itemData.quantity > 0 ? '' : 'sellout')}>
+                    <div className="activityItem">
+                        <div className="activityItemsImg">
+                            {
+                                itemData.imagePath ?
+                                    <img src={ domain + util.getMiddlePic(itemData.imagePath) }/> :
+                                    <img src={defProductImg}/>
 
-                        }
+                            }
 
+                        </div>
+                        <div className="activityItemsInfo">
+                            <p className="font12">{itemData.brandName}</p>
+                            <p className="font14">{itemData.shortName}</p>
+                            <p className="font12">{attr}</p>
+                        </div>
+                        <div className="activityItemsMoney font18">
+                            {itemData.sellprice/100 || 0 } <span className="font12">元</span>
+                        </div>
+                        {this.props.children}
                     </div>
-                    <div className="activityItemsInfo">
-                        <p className="font12">{itemData.brandName}</p>
-                        <p className="font14">{itemData.shortName}</p>
-                        <p className="font12">{attr}</p>
+                    {itemTag}
+                    {itemDesc}
+                    <div className="activityLayer">
+                        <div className="sellouts font14">缺货</div>
                     </div>
-                    <div className="activityItemsMoney font18">
-                        {itemData.sellprice/100 || 0 } <span className="font12">元</span>
-                    </div>
-                    {this.props.children}
                 </div>
-                {itemTag}
-                {itemDesc}
-                <div className="activityLayer">
-                    <div className="sellouts font14">缺货</div>
-                </div>
-            </div>
-        )
+            )
+        }
+
     }
 }
 
