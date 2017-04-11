90d10a18fe62406bdc39d59df427120ab7afef24
Ruibing <ruibing@Ruibings-MacBook-Pro.local>
2017-04-11 12:50:11 +0800

    debug pay

--- Customer_Pages/client/containers/ConfirmOrderContainer/confirmOrderContainer.jsx
+++ Customer_Pages/client/containers/ConfirmOrderContainer/confirmOrderContainer.jsx
@@ -19,7 +19,7 @@ class ConfirmOrderContainer extends React.Component {
         const { dispatch,state } = this.props;
         const link = state.weixin.wechat_url;
         let wlink = 'http://www.mjitech.com/buyer_pages/index.html#/confirmOrder';
-        dispatch(initWxConfig(wlink,initOrderConfirm(this._orderNumber)));
+        dispatch(initWxConfig(window.location.href,initOrderConfirm(this._orderNumber)));
         // this._isAndroid ?
         //     dispatch(initWxConfig(link,initOrderConfirm(this._orderNumber))):
         //     dispatch(initOrderConfirm(this._orderNumber));
