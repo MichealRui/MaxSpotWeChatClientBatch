e974c92612bff9ca6bb06ffc1e7a2f018ecc1cb7
cabbage <403791823@qq.com>
2017-08-17 18:57:15 +0800

    bug

--- Customer_Pages/client/reducers/Loading.js
+++ Customer_Pages/client/reducers/Loading.js
@@ -3,14 +3,19 @@
  */
 import * as actionTypes from '../actionTypes/common/Loading'
 
-function toggleStatue(content) {
-    return Object.assign({}, content)
+function toggleStatue(state,content) {
+    if(state.status){
+        return Object.assign({}, state,content)
+    }else{
+        return Object.assign({}, state)
+    }
+
 }
 
 export default function(state={status:true}, action) {
     switch (action.type) {
         case actionTypes.TOGGLE_STATUS :
-            return toggleStatue(action.content);
+            return toggleStatue(state,action.content);
         default:
             return state;
     }
