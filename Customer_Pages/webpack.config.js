55a76d57753350af041e119e0acc3310c7dda82a
cabbage <403791823@qq.com>
2017-08-20 23:34:30 +0800

    test

--- Customer_Pages/webpack.config.js
+++ Customer_Pages/webpack.config.js
@@ -20,7 +20,7 @@ var baseConfig = {
     loaders: [
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&minetype=application/font-woff"
+        loader: "url-loader?minetype=application/font-woff"
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
